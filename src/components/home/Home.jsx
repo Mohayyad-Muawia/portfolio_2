0bf45a1a7f7e6be97b2c6b9b67855a6b78d93221
Mohayyad Muawia <mohayyad2.0@gmail.com>
2025-06-10 14:33:15 +0200

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,3 @@
-import { get_all_projects } from "@/lib/actions";
 import About from "./home components/About";
 import Hero from "./home components/Hero";
 import Services from "./home components/Services";
@@ -6,27 +5,15 @@ import "./home.css";
 import Portfolio from "./home components/Portfolio";
 import Contact from "./home components/Contact";
 import Welcome from "../Welcome";
-import toast from "react-hot-toast";
 
-export const dynamic = "force-dynamic";
 export default async function Home() {
-  let allProjects;
-  try {
-    allProjects = await get_all_projects();
-    if (!allProjects) {
-      toast.error("Failed to fetch projects");
-    }
-  } catch (err) {
-    console.log(err);
-  }
-
   return (
     <div className="home">
       <Welcome />
       <Hero />
       <About />
       <Services />
-      <Portfolio allProjects={allProjects} />
+      <Portfolio />
       <Contact />
     </div>
   );
